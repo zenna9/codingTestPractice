da747d91d3006a7ad05a4dcc88db100a3bcb1e43
zenna <97446606+zenna9@users.noreply.github.com>
2025-04-09 23:43:47 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.76 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,27 +1,22 @@
 function solution(board, moves) {
     const basket = [];
     let answer = 0;
-    const newBoard = Array.from({ length: board.length }, () => []) 
+    const newBoard = Array.from({ length: board.length }, () => []);
+    
     board.map((가로줄, 가로줄index)=>{
-        가로줄.map((칸,칸index)=>{ // [0,0,1,0,3], 1
+        가로줄.map((칸,칸index)=>{ 
             if(칸){ // 0이아니면
                 newBoard[칸index].push(칸);
             }
         })
     });
     
-    console.log(newBoard)
-    
     moves.map((move)=>{
-        const 행 = newBoard[move-1];
+        const 세로 = newBoard[move-1];
+        if(세로.length === 0 ) return;
         
-        if(행.length === 0 ) return;
-        
-        const doll = 행.shift();
-        console.log(`행 : ${행}`);
-        console.log(`doll : ${doll}`);
-        
-        
+        const doll = 세로.shift();
+            
         if(basket[basket.length-1] === doll){
             basket.pop();
             answer += 2;
@@ -30,6 +25,6 @@ function solution(board, moves) {
         }
         
     })
-    // console.log(newBoard);
+    
     return answer;
 }
