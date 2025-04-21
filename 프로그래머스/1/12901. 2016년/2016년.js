3f2325888f3e796f298fe50f01185c08cc3ccb4d
zenna <97446606+zenna9@users.noreply.github.com>
2025-04-22 00:40:39 +0900

    [level 1] Title: 2016년, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,13 +1,13 @@
 function solution(a, b) {
-    const newYear = new Date('2016-01-01') ;
-    const chkDay = new Date(`2016-${String(a).padStart(2,"0")}-${String(b).padStart(2,"0")}`) ;
-
-    const minus = ((chkDay - newYear)/1000/60/60/24) ;
-    const DAY = ["FRI","SAT","SUN","MON","TUE","WED","THU"];
+    const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+    const datesInMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     
-    return DAY[minus%7];
+    let dateCount = 0;
+    for (let i = 0; i < a - 1; i++) {
+        dateCount += datesInMonth[i];
+    }
+    dateCount += b;    
+    
+    // 2016/1/1이 금요일이므로 days index 시작점 맞추기 위해 +4
+    return days[(dateCount + 4) % 7];
 }
-
-// new Date("2025-4-1") 처럼 월과 일을 2자리수로 맞추지 않으면 브라우저에 따라 잘못된 날짜를 생성한다는 것을 알게 되었다.
-
-// 두 Date 사이의 날짜를 구하고 싶으면 : 차이/1000/60/60/24 (밀리초, 초, 분, 시)
