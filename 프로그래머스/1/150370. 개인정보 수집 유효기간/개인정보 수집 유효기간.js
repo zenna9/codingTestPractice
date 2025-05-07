049c8c02bc057a27a8e1513695ed1dfd9d228b92
zenna <97446606+zenna9@users.noreply.github.com>
2025-05-07 21:49:03 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 6.72 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -0,0 +1,24 @@
+function solution(today, terms, privacies) {
+    const answer = []; 
+    const [year, month, day] = today.split('.');
+    const expire = new Date(today);
+    const termType = {};
+    
+    terms.forEach((item) => {
+        const [type, term] = item.split(' ');
+        termType[type] = Number(term);    
+      
+    })
+    
+    privacies.forEach((item, index) => {
+        const [date, type] = item.split(' ');
+        const chDate = new Date(date);
+        
+        chDate.setMonth(chDate.getMonth() + termType[type]);
+        
+        if(chDate <= expire) answer.push(index + 1 );
+    })
+    
+    return answer
+    
+}
