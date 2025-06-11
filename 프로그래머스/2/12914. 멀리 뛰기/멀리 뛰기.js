dbfc8060d2169f8d18c816966966c2cace6f06d6
zenna <97446606+zenna9@users.noreply.github.com>
2025-06-11 20:29:18 +0900

    update code

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,13 +1,15 @@
 function solution(n) {
     if(n===1) return 1;
     if(n===2) return 2;
+    
     const wayCnt = new Array(n).fill(0);
+    
     wayCnt[0] = 1;
     wayCnt[1] = 2;
+    
     for(let i =2 ; i < n ; i++){
-        wayCnt[i] = (wayCnt[i-1]+wayCnt[i-2])%1234567;
+        wayCnt[i] = (wayCnt[i-1]+wayCnt[i-2])%1234567; // 너무 큰 값 방지
     }
-    console.log(wayCnt);
-    return wayCnt.at(-1)
-    // return (2**몫+더할값+(몫-1))%1234567;
+    
+    return wayCnt.at(-1); 
 }
