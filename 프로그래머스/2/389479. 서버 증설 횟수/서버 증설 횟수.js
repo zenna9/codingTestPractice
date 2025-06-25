8d03d4167672d3a620cc8521c996b34170a58459
zenna9 <97446606+zenna9@users.noreply.github.com>
2025-06-25 20:29:30 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.js
@@ -0,0 +1,19 @@
+function solution(players, m, k) {
+  let answer = 0;
+  const server = new Array(24).fill(0);
+
+  for (let i = 0; i < 24; i++) {
+    const required = Math.ceil(players[i] / m);
+    const current = server[i];
+    const need = required - current;
+
+    if (need > 0) {
+      answer += need;
+      for (let j = i; j < Math.min(24, i + k); j++) {
+        server[j] += need;
+      }
+    }
+  }
+
+  return answer;
+}
